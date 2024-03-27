f66300677416a937e43a75850e75eb73f4aa2992
brandonhach <specialcambo@gmail.com>
2024-03-27 16:49:47 -0400

    signup hashed with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
 	firstName: { type: String, required: [true, 'cannot be empty'] },
 	lastName: { type: String, required: [true, 'cannot be empty'] },
 	email: { type: String, required: [true, 'cannot be empty'], unique: true },
 	password: { type: String, required: [true, 'cannot be empty'] },
+});
+
+// replace plaintext password with hashed password before saving the document in the db
+// pre middleware
+userSchema.pre('save', function (next) {
+	let user = this;
+	if (!user.isModified('password')) return next();
+	bcrypt
+		.hash(user.password, 10)
+		.then((hash) => {
+			user.password = hash;
+			next();
+		})
+		.catch((err) => next(err));
 });
 
 module.exports = mongoose.model('User', userSchema);
