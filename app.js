d3b81c9c63e063395f83bea00de6581c15f8eab6
brandonhach <specialcambo@gmail.com>
2024-03-27 16:29:55 -0400

    user model setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const User = require('./models/user');
 
 //create app
 const app = express();
@@ -12,40 +13,42 @@ let host = 'localhost';
 app.set('view engine', 'ejs');
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/demos', 
-                {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-.then(()=>{
-    app.listen(port, host, ()=>{
-        console.log('Server is running on port', port);
-    });
-})
-.catch(err=>console.log(err.message));
+mongoose
+	.connect('mongodb://localhost:27017/demos', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		app.listen(port, host, () => {
+			console.log('Server is running on port', port);
+		});
+	})
+	.catch((err) => console.log(err.message));
 
 //mount middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
 //set up routes
-app.get('/', (req, res)=>{
-    res.render('index');
+app.get('/', (req, res) => {
+	res.render('index');
 });
-
 
 app.use((req, res, next) => {
-    let err = new Error('The server cannot locate ' + req.url);
-    err.status = 404;
-    next(err);
-
+	let err = new Error('The server cannot locate ' + req.url);
+	err.status = 404;
+	next(err);
 });
 
-app.use((err, req, res, next)=>{
-    console.log(err.stack);
-    if(!err.status) {
-        err.status = 500;
-        err.message = ("Internal Server Error");
-    }
+app.use((err, req, res, next) => {
+	console.log(err.stack);
+	if (!err.status) {
+		err.status = 500;
+		err.message = 'Internal Server Error';
+	}
 
-    res.status(err.status);
-    res.render('error', {error: err});
+	res.status(err.status);
+	res.render('error', { error: err });
 });
